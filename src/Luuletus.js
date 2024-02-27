e64b83374575a539fb92bb7d76d6f708d78359c3
irisahah <irisharjo@gmail.com>
2024-02-27 19:41:08 +0200

    Initial commit

---
+++ src/Luuletus.js
@@ -0,0 +1,10 @@
+function Luuletus() {
+    return (
+        <div>
+            <p>Kes hommkuti külas käib,</p>
+            <p>see asjatult ei longi</p>
+        </div>
+    );
+}
+
+export default Luuletus
